7f2baf3aeafa2000adb46dc5ef3b075600bbf6dc
Kevin Omidvaran <omidkv@unm.edu>
2017-04-01 00:13:54 -0600

    two teams players added

---
+++ 2ndHalfPlayer.sql
@@ -0,0 +1,85 @@
+
+
+insert into teams values('Manchester United',1878,'Old Trafford', 'Manchester United plc');
+
+insert into players values('David de Gea', 1, 'Manchester United', 'Spain', 'GoalKeeper');
+insert into players values('Sergio Romero', 20, 'Manchester United', 'Argentina', 'GoalKeeper');
+insert into players values('Joel Pereira', 40, 'Manchester United', 'Portugal', 'GoalKeeper');
+insert into players values('Dean Henderson', 34, 'Manchester United', 'England', 'GoalKeeper');
+insert into players values('Phil Jones', 4, 'Manchester United', 'England', 'Defender');
+insert into players values('Marcos Rojo', 5, 'Manchester United', 'Argentina', 'Defender');
+insert into players values('Chris Smalling', 12, 'Manchester United', 'England', 'Defender');
+insert into players values('Daley Blind', 17, 'Manchester United', 'Netherlands', 'Defender');
+insert into players values('Luke Shaw', 23, 'Manchester United', 'England', 'Defender');
+insert into players values('Antonio Valencia', 25, 'Manchester United', 'Ecuador', 'Defender');
+insert into players values('Matteo Darmian', 36, 'Manchester United', 'Italy', 'Defender');
+insert into players values('Timothy Fosu-Mensah', 24, 'Manchester United', 'Netherlands', 'Defender');
+insert into players values('Eric Bailly', 3, 'Manchester United', 'Cote DIvoire', 'Defender');
+insert into players values('Axel Tuanzebe', 38, 'Manchester United', 'England', 'Defender');
+insert into players values('Juan Mata', 8 , 'Manchester United', 'Spain', 'Midfielder');
+insert into players values('Michael Carrick', 16, 'Manchester United', 'England', 'Midfielder');
+insert into players values('Ashley Young',18 , 'Manchester United', 'England', 'Midfielder');
+insert into players values('Ander Herrera', 21, 'Manchester United', 'Spain', 'Midfielder');
+insert into players values('Marouane Fellaini', 27, 'Manchester United', 'Belgium', 'Midfielder');
+insert into players values('Jesse Lingard', 14, 'Manchester United', 'England', 'Midfielder');
+insert into players values('Henrikh Mkhitaryan', 22, 'Manchester United', 'Armenia', 'Midfielder');
+insert into players values('Paul Pogba', 6, 'Manchester United', 'France', 'Midfielder');
+insert into players values('Anthony Martial', 11, 'Manchester United', 'France', 'Foward');
+insert into players values('Wayne Rooney', 10, 'Manchester United', 'England', 'Foward');
+insert into players values('Marcus Rashford', 19, 'Manchester United', 'England', 'Foward');
+insert into players values('Zlatan Ibrahimovic', 9, 'Manchester United', 'Sweden', 'Foward');
+
+insert into teams values ('Middlesbrough',1876, 'Riverside Stadium', 'Steve Gibson');
+
+insert into players values('Dimitrios Konstantopoulos', 1, 'Middlesbrough', 'Greece', 'GoalKeeper');
+insert into players values('Victor Valdes', 26, 'Middlesbrough', 'Spain', 'GoalKeeper');
+insert into players values('Brad Guzan', 12, 'Middlesbrough', 'United States', 'GoalKeeper');
+insert into players values('George Friend', 3, 'Middlesbrough', 'England', 'Defender');
+insert into players values('Daniel Ayala', 4, 'Middlesbrough', 'Spain', 'Defender');
+insert into players values('Ben Gibson', 6, 'Middlesbrough', 'England', 'Defender');
+insert into players values('Bernardo', 5, 'Middlesbrough', 'Colombia', 'Defender');
+insert into players values('Antonio Barragan', 17, 'Middlesbrough', 'Spain', 'Defender');
+insert into players values('Fabio', 2, 'Middlesbrough', 'Brazil', 'Defender');
+insert into players values('James Husband', 40, 'Middlesbrough', 'England', 'Defender');
+insert into players values('Calum Chambers', 25, 'Middlesbrough', 'England', 'Defender');
+insert into players values('Dael Fry', 22, 'Middlesbrough', 'England', 'Defender');
+insert into players values('Grant Leadbitter', 7, 'Middlesbrough', 'England', 'Midfielder');
+insert into players values('Adam Clayton', 8, 'Middlesbrough', 'England', 'Midfielder');
+insert into players values('Stewart Downing', 19, 'Middlesbrough', 'England', 'Midfielder');
+insert into players values('Adam Forshaw', 34, 'Middlesbrough', 'England', 'Midfielder');
+insert into players values('Marten de Roon', 14, 'Middlesbrough', 'Netherlands', 'Midfielder');
+insert into players values('Adama Traore', 37, 'Middlesbrough', 'Spain', 'Midfielder');
+insert into players values('Gaston Ramirez', 21, 'Middlesbrough', 'Uruguay', 'Midfielder');
+insert into players values('Adlene Guedioura', 27, 'Middlesbrough', 'Algeria', 'Midfielder');
+insert into players values('Cristhian Stuani', 18, 'Middlesbrough', 'Uruguay', 'Foward');
+insert into players values('Viktor Fischer', 11, 'Middlesbrough', 'Denmark', 'Foward');
+insert into players values('Alvaro Negredo', 10, 'Middlesbrough', 'Spain', 'Foward');
+insert into players values('Rudy Gestede', 29, 'Middlesbrough', 'Benin', 'Foward');
+
+insert into teams values ('Southampton', 1885, 'St Mary\'s Stadium', 'Katharina Liebherr');
+
+insert into teams values ('Stoke City', 1863, 'bet365 Stadium', 'bet365');
+insert into teams values ('Sunderland', 1879, 'Stadium of Light', 'Ellis Short');
+insert into teams values ('Swansea City', 1912, 'Liberty Stadium', 'Jason Levien');
+insert into teams values('Tottenham Hotspur',1882,'White Hart Lane', 'ENIC International Ltd.');
+insert into teams values ('Watford', 1881, 'Vicarage Road', 'Gino Pozzo');
+insert into teams values ('West Bromwich Albion', 1878, 'The Hawthorns', 'Guochuan Lai');
+insert into teams values ('West Ham United', 1895, 'London Stadium', 'David Sullivan');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
