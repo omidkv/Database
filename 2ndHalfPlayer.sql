3f6926bf4592e2f8e11c3b1b9f8ad0f126e57fdf
Kevin Omidvaran <omidkv@unm.edu>
2017-04-01 20:27:28 -0600

    five teams players added

--- 2ndHalfPlayer.sql
+++ 2ndHalfPlayer.sql
@@ -24,10 +24,10 @@ insert into players values('Marouane Fellaini', 27, 'Manchester United', 'Belgiu
 insert into players values('Jesse Lingard', 14, 'Manchester United', 'England', 'Midfielder');
 insert into players values('Henrikh Mkhitaryan', 22, 'Manchester United', 'Armenia', 'Midfielder');
 insert into players values('Paul Pogba', 6, 'Manchester United', 'France', 'Midfielder');
-insert into players values('Anthony Martial', 11, 'Manchester United', 'France', 'Foward');
-insert into players values('Wayne Rooney', 10, 'Manchester United', 'England', 'Foward');
-insert into players values('Marcus Rashford', 19, 'Manchester United', 'England', 'Foward');
-insert into players values('Zlatan Ibrahimovic', 9, 'Manchester United', 'Sweden', 'Foward');
+insert into players values('Anthony Martial', 11, 'Manchester United', 'France', 'Forward');
+insert into players values('Wayne Rooney', 10, 'Manchester United', 'England', 'Forward');
+insert into players values('Marcus Rashford', 19, 'Manchester United', 'England', 'Forward');
+insert into players values('Zlatan Ibrahimovic', 9, 'Manchester United', 'Sweden', 'Forward');
 
 insert into teams values ('Middlesbrough',1876, 'Riverside Stadium', 'Steve Gibson');
 
@@ -51,17 +51,179 @@ insert into players values('Marten de Roon', 14, 'Middlesbrough', 'Netherlands',
 insert into players values('Adama Traore', 37, 'Middlesbrough', 'Spain', 'Midfielder');
 insert into players values('Gaston Ramirez', 21, 'Middlesbrough', 'Uruguay', 'Midfielder');
 insert into players values('Adlene Guedioura', 27, 'Middlesbrough', 'Algeria', 'Midfielder');
-insert into players values('Cristhian Stuani', 18, 'Middlesbrough', 'Uruguay', 'Foward');
-insert into players values('Viktor Fischer', 11, 'Middlesbrough', 'Denmark', 'Foward');
-insert into players values('Alvaro Negredo', 10, 'Middlesbrough', 'Spain', 'Foward');
-insert into players values('Rudy Gestede', 29, 'Middlesbrough', 'Benin', 'Foward');
+insert into players values('Cristhian Stuani', 18, 'Middlesbrough', 'Uruguay', 'Forward');
+insert into players values('Viktor Fischer', 11, 'Middlesbrough', 'Denmark', 'Forward');
+insert into players values('Alvaro Negredo', 10, 'Middlesbrough', 'Spain', 'Forward');
+insert into players values('Rudy Gestede', 29, 'Middlesbrough', 'Benin', 'Forward');
 
 insert into teams values ('Southampton', 1885, 'St Mary\'s Stadium', 'Katharina Liebherr');
 
+insert into players values('Fraser Forster',1,'Southampton','England','GoalKeeper');
+insert into players values('Alex McCarthy',13,'Southampton','England','GoalKeeper');
+insert into players values('Stuart Taylor',28,'Southampton','England','GoalKeeper');
+insert into players values('Harry Lewis',41,'Southampton','England','GoalKeeper');
+insert into players values('Mouez Hassen',40,'Southampton','France','GoalKeeper');
+insert into players values('Cedric Soares',2,'Southampton','Portugal','Defender');
+insert into players values('Maya Yoshida',3,'Southampton','Japan','Defender');
+insert into players values('Florin Gardos',5,'Southampton','Romania','Defender');
+insert into players values('Cuco Martina',15,'Southampton','Curacao','Defender');
+insert into players values('Virgil van Dijk',17,'Southampton','Netherlands','Defender');
+insert into players values('Matt Targett',33,'Southampton','England','Defender');
+insert into players values('Jeremy Pied',26,'Southampton','France','Defender');
+insert into players values('Jack Stephens'24,,'Southampton','England','Defender');
+insert into players values('Ryan Bertrand',21,'Southampton','England','Defender');
+insert into players values('Martin Caceres',12,'Southampton','Uruguay','Defender');
+insert into players values('Jordy Clasie',4,'Southampton','Netherlands','Midfielder');
+insert into players values('Steven Davis',8,'Southampton','Northern Ireland','Midfielder');
+insert into players values('usan Tadic',11,'Southampton','Serbia','Midfielder');
+insert into players values('Oriol Romeu',14,'Southampton','Spain','Midfielder');
+insert into players values('James Ward-Prowse',16,'Southampton','England','Midfielder');
+insert into players values('Harrison Reed',18,'Southampton','England','Midfielder');
+insert into players values('Nathan Redmond',22,'Southampton','England','Midfielder');
+insert into players values('Pierre-Emile Hojbjerg',23,'Southampton','Denmark','Midfielder');
+insert into players values('Sofiane Boufal',19,'Southampton','Morocco','Midfielder');
+insert into players values('Lloyd Isgrove',27,'Southampton','Wales','Midfielder');
+insert into players values('Jake Hesketh',42,'Southampton','England','Midfielder');
+insert into players values('Sam McQueen',38,'Southampton','England','Midfielder');
+insert into players values('Josh Sims',39,'Southampton','England','Midfielder');
+insert into players values('Shane Long',7,'Southampton','Ireland','Forward');
+insert into players values('Jay Rodriquez',9,'Southampton','England','Forward');
+insert into players values('Charlie Austin',10,'Southampton','England','Forward');
+insert into players values('Olufela Olomola',32,'Southampton','Nigeria','Forward');
+insert into players values('Manolo Gabbiadini',20,'Southampton','Italy','Forward');
+
 insert into teams values ('Stoke City', 1863, 'bet365 Stadium', 'bet365');
+
+insert into players values('Jack Butland',1,'Stoke City','England','GoalKeeper');
+insert into players values('Shay Given',24,'Stoke City','Ireland','GoalKeeper');
+insert into players values('Daniel Bachmann',35,'Stoke City','Austria','GoalKeeper');
+insert into players values('Phil Bardsley',2,'Stoke City','Scotland','Defender');
+insert into players values('Erik Pieters',3,'Stoke City','Netherlands','Defender');
+insert into players values('Marc Muniesa',5,'Stoke City','Spain','Defender');
+insert into players values('Glen Johnson',8,'Stoke City','England','Defender');
+insert into players values('Ryan Shawcross',17,'Stoke City','England','Defender');
+insert into players values('Geoff Cameron',20,'Stoke City','United States','Defender');
+insert into players values('Bruno Martins Indi',15,'Stoke City','Netherlands','Defender');
+insert into players values('Thomas Edwards',72,'Stoke City','England','Defender');
+insert into players values('Glenn Whelan',6,'Stoke City','Ireland','Midfielder');
+insert into players values('Stephen Ireland',7,'Stoke City','Ireland','Midfielder');
+insert into players values('Ibrahim Afellay',14,'Stoke City','Netherlands','Midfielder');
+insert into players values('Charlie Adam',16,'Stoke City','Scotland','Midfielder');
+insert into players values('Giannelli Imbula',21,'Stoke City','France','Midfielder');
+insert into players values('Xherdan Shaqiri',22,'Stoke City','Switzerland','Midfielder');
+insert into players values('Ramadan Sobhi',32,'Stoke City','Egypt','Midfielder');
+insert into players values('Joe Allen',4,'Stoke City','Wales','Midfielder');
+insert into players values('Marko Arnautovic',10,'Stoke City','Austria','Forward');
+insert into players values('Mame Biram Diouf',18,'Stoke City','Senegal','Forward');
+insert into players values('Jonathan Walters',19,'Stoke City','Ireland','Forward');
+insert into players values('Peter Crouch',25,'Stoke City','England','Forward');
+insert into players values('Julien Ngoy',45,'Stoke City','Belgium','Forward');
+insert into players values('Wilfried Bony',12,'Stoke City','Cote DIvoire','Forward');
+insert into players values('Thibaud Verlinden',55,'Stoke City','Belgium','Forward');
+
+
 insert into teams values ('Sunderland', 1879, 'Stadium of Light', 'Ellis Short');
+
+insert into players values('Jordan Pickford',13,'Sunderland','England','GoalKeeper');
+insert into players values('Vito Mannone',1,'Sunderland','Italy','GoalKeeper');
+insert into players values('Max Stryjek',32,'Sunderland','Poland','GoalKeeper');
+insert into players values('Mika',12,'Sunderland','Portugal','GoalKeeper');
+insert into players values('Billy Jones',2,'Sunderland','England','Defender');
+insert into players values('John OShea',16,'Sunderland','Ireland','Defender');
+insert into players values('Lamine Kone',23,'Sunderland','Cote DIvoire','Defender');
+insert into players values('Papy Dijilobodji',,'Sunderland','Senegal','Defender');
+insert into players values('Donald Love',22,'Sunderland','Scotland','Defender');
+insert into players values('Paddy McNair',19,'Sunderland','Northern Ireland','Defender');
+insert into players values('Javier Manquillo',21,'Sunderland','Spain','Defender');
+insert into players values('Josh Robson',45,'Sunderland','England','Defender');
+insert into players values('Jason Denayer',4,'Sunderland','Belgium','Defender');
+insert into players values('Joleon Lescott',15,'Sunderland','England','Defender');
+insert into players values('Bryan Oviedo',3,'Sunderland','Costa Rica','Defender');
+insert into players values('Jan Kirchhoff',27,'Sunderland','Germany','Midfielder');
+insert into players values('Sebastian Larsson',7,'Sunderland','Sweden','Midfielder');
+insert into players values('Lee Cattermole',6,'Sunderland','England','Midfielder');
+insert into players values('Jack Rodwell',8,'Sunderland','England','Midfielder');
+insert into players values('Wahbi Khazri',10,'Sunderland','Tunisia','Midfielder');
+insert into players values('Adnan Januzaj',44,'Sunderland','Belgium','Midfielder');
+insert into players values('Steven Pienaar',20,'Sunderland','South Africa','Midfielder');
+insert into players values('Lynden Gooch',46,'Sunderland','United States','Midfielder');
+insert into players values('Ethan Robson',25,'Sunderland','England','Midfielder');
+insert into players values('Rees Greenwood',37,'Sunderland','England','Midfielder');
+insert into players values('George Honeyman',39,'Sunderland','England','Midfielder');
+insert into players values('Didier Ndong',17,'Sunderland','Gabon','Midfielder');
+insert into players values('Elliot Embleton',40,'Sunderland','England','Midfielder');
+insert into players values('Fabio Borini',9,'Sunderland','Italy','Forward');
+insert into players values('Jermain Defoe',18,'Sunderland','England','Forward');
+insert into players values('Duncan Watmore',14,'Sunderland','England','Forward');
+insert into players values('Joel Asoro',29,'Sunderland','Sweden','Forward');
+insert into players values('Victor Anichebe',28,'Sunderland','Nigeria','Forward');
+insert into players values('Josh Maja',35,'Sunderland','England','Forward');
+
+
 insert into teams values ('Swansea City', 1912, 'Liberty Stadium', 'Jason Levien');
+
+insert into players values('Jordan Ayew',3,'Swansea City','Ghana','Forward');
+insert into players values('Oliver McBurnie',62,'Swansea City','Scotland','Forward');
+insert into players values('Fernando Llorente',9,'Swansea City','Spain','Forward');
+insert into players values('Borja Baston',10,'Swansea City','Spain','Forward');
+insert into players values('Lukasz Fabianski',1,'Swansea City','Poland','GoalKeeper');
+insert into players values('Kristoffer Nordfeldt',13,'Swansea City','Sweden','GoalKeeper');
+insert into players values('Mark Birighitti',19,'Swansea City','Australia','GoalKeeper');
+insert into players values('Gerhard Tremmel',25,'Swansea City','Germany','GoalKeeper');
+insert into players values('Jordi Amat',2,'Swansea City','Spain','Defender');
+insert into players values('Angel Rangel',22,'Swansea City','Spain','Defender');
+insert into players values('Kyle Naughton',26,'Swansea City','England','Defender');
+insert into players values('Federico Fernandez',33,'Swansea City','Argentina','Defender');
+insert into players values('Mike van der Hoorn',5,'Swansea City','Netherlands','Defender');
+insert into players values('Alfie Mawson',6,'Swansea City','England','Defender');
+insert into players values('Stephen Kingsley',35,'Swansea City','Scotland','Defender');
+insert into players values('Martin Olsson',16,'Swansea City','Sweden','Defender');
+insert into players values('Connor Roberts',24,'Swansea City','Wales','Defender');
+insert into players values('Ki Sung-yueng',4,'Swansea City','South Korea','Midfielder');
+insert into players values('Leon Britton',7,'Swansea City','England','Midfielder');
+insert into players values('Nathan Dyer',12,'Swansea City','England','Midfielder');
+insert into players values('Wayne Routledge',15,'Swansea City','England','Midfielder');
+insert into players values('Jefferson Montero',20,'Swansea City','Ecuador','Midfielder');
+insert into players values('Gylfi Sigurdsson',23,'Swansea City','Iceland','Midfielder');
+insert into players values('Jack Cork',24,'Swansea City','England','Midfielder');
+insert into players values('Jay Fulton',56,'Swansea City','Scotland','Midfielder');
+insert into players values('Leroy Fer',8,'Swansea City','Netherlands','Midfielder');
+insert into players values('Daniel James',51,'Swansea City','Wales','Midfielder');
+insert into players values('Luciano Narsingh',28,'Swansea City','Netherlands','Midfielder');
+insert into players values('Tom Carroll',42,'Swansea City','England','Midfielder');
+
+
 insert into teams values('Tottenham Hotspur',1882,'White Hart Lane', 'ENIC International Ltd.');
+
+insert into players values('Hugo Lloris',1,'Tottenham Hotspur','France','GoalKeeper');
+insert into players values('Michel Vorm',13,'Tottenham Hotspur','Netherlands','GoalKeeper');
+insert into players values('Pau Lopez',30,'Tottenham Hotspur','Spain','GoalKeeper');
+insert into players values('Alfie Whiteman',41,'Tottenham Hotspur','England','GoalKeeper');
+insert into players values('Kyle Walker',2,'Tottenham Hotspur','England','Defender');
+insert into players values('Danny Rose',3,'Tottenham Hotspur','England','Defender');
+insert into players values('Toby Alderweireld',4,'Tottenham Hotspur','Belgium','Defender');
+insert into players values('Jan Vertonghen',5,'Tottenham Hotspur','Belgium','Defender');
+insert into players values('Kieran Trippier',16,'Tottenham Hotspur','England','Defender');
+insert into players values('Kevin Wimmer',27,'Tottenham Hotspur','Austria','Defender');
+insert into players values('Ben Davis',33,'Tottenham Hotspur','Wales','Defender');
+insert into players values('Cameron Carter-Vickers',38,'Tottenham Hotspur','United States','Defender');
+insert into players values('Kyle Walker-Peters',37,'Tottenham Hotspur','England','Defender');
+insert into players values('Son Heung-Min',7,'Tottenham Hotspur','South Korea','Midfielder');
+insert into players values('Erik Lamela',11,'Tottenham Hotspur','Argentina','Midfielder');
+insert into players values('Eric Dier',15,'Tottenham Hotspur','England','Midfielder');
+insert into players values('Mousa Dembele',19,'Tottenham Hotspur','Belgium','Midfielder');
+insert into players values('Dele Alli',20,'Tottenham Hotspur','England','Midfielder');
+insert into players values('Victor Wanyama',12,'Tottenham Hotspur','Kenya','Midfielder');
+insert into players values('Christian Eriksen',23,'Tottenham Hotspur','Denmark','Midfielder');
+insert into players values('Josh Onomah',25,'Tottenham Hotspur','England','Midfielder');
+insert into players values('Harry Winks',29,'Tottenham Hotspur','England','Midfielder');
+insert into players values('Moussa Sissoko',17,'Tottenham Hotspur','France','Midfielder');
+insert into players values('Georges-Kevin Nkoudou',14,'Tottenham Hotspur','France','Midfielder');
+insert into players values('Marcus Edwards',48,'Tottenham Hotspur','England','Midfielder');
+insert into players values('Harry Kane',10,'Tottenham Hotspur','England','Forward');
+insert into players values('Vincent Janssen',9,'Tottenham Hotspur','Netherlands','Forward');
+
+
 insert into teams values ('Watford', 1881, 'Vicarage Road', 'Gino Pozzo');
 insert into teams values ('West Bromwich Albion', 1878, 'The Hawthorns', 'Guochuan Lai');
 insert into teams values ('West Ham United', 1895, 'London Stadium', 'David Sullivan');
